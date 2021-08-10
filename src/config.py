c4d95c7f3dc8be7d0bea79c7c6e77fba87017ebf
Anastasia Kozlova <anastasi2510@gmail.com>
2021-08-10 10:42:00 +0300

    Updated questions detection among comments

--- src/config.py
+++ src/config.py
@@ -6,5 +6,6 @@ ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent
 # data dir with datasets
 DATA_DIR = os.path.join(ROOT_DIR, "data")
 
-COMMENTS_FILE_NAME = "AN-695-Egor-COMMENTS.csv.gz"
+COMMENTS_FILE_NAME = "COMMENTS.csv.gz"
 STEPS_FILE_NAME = "AN-695-Egor-STEPS.csv.gz"
+COURSES_FILE_NAME = "popular_courses.csv"
--- src/load_data.py
+++ src/load_data.py
@@ -1,7 +1,24 @@
+import re
 import pandas as pd
+import numpy as np
+
+pattern_courses = r"код|компьютерн|математи|информатик|программир|алгебр|физик|хими|unix|python|excel|linux"
 
 
 def load_data(filepath: str) -> pd.DataFrame:
     df = pd.read_csv(filepath, compression="gzip")
+    df = df[df.text.notna()].reset_index(drop=True)
     df["text"] = df["text"].astype(str)
     return df
+
+
+def load_courses(filepath_courses):
+    df_courses = pd.read_csv(filepath_courses)
+    df_courses = df_courses[df_courses.language == "ru"]
+    df_courses["use_problem"] = np.logical_or.reduce(
+        [
+            df_courses[c].str.contains(pattern_courses, flags=re.IGNORECASE).fillna(False)
+            for c in ["title", "description", "requirements", "summary", "target_audience"]
+        ]
+    )
+    return df_courses[["course_id", "use_problem"]].reset_index(drop=True)
